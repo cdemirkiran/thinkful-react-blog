ee03aa5436d2f89a69c8beb6f43d40764fa1f7f4
Jason humphrey <jason.humphrey@live.com>
2015-10-22 23:31:09 -0500

    Update to bootstrap

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,9 +16,21 @@ export default React.createClass({
 
   render() {
     return (
-      <div>
+      <div className="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">React JS Blog
+                    <small> by Green Pioneer & Harry </small>
+                </h1>
+            </div>
+        </div>
         <PostsList
           posts={ this.state.posts }/>
+        <div className="row">
+            <div className="col-lg-12">
+                <h1 className="page-header">Create A New Post</h1>
+            </div>
+        </div>
         <NewPostContainer 
           title={ this.state.title }
           content={ this.state.content }
